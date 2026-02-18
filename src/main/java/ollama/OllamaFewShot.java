4cc0becfbc34bac084a5c8a82905ba3146ca66db
jmferreira <jmferreira@dockerhost>
2026-02-18 11:35:28 -0300

    -Ejemplo de few shots

---
+++ src/main/java/ollama/OllamaFewShot.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ollama;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ *
+ * @author jmferreira
+ */
+public class OllamaFewShot {
+
+    public static void main(String[] args) {
+
+        OllamaStreamingChatService chatService
+                = new OllamaStreamingChatService(
+                        "http://10.254.3.242:11434",
+                        "gemma3:4b"
+                );
+        List<ChatMessage> fewShotHistory = new ArrayList<>();
+        
+        // Adding positive feedback example to history
+        fewShotHistory.add(UserMessage.from(
+                "Me compré unos calzados Nike por 85 dólares en el centro."));
+        fewShotHistory.add(AiMessage.from(
+                "Producto: Calzado Nike, Precio: 85, Moneda: USD, Sentimiento: NEUTRO"));
+
+        // Adding negative feedback example to history
+        fewShotHistory.add(UserMessage
+                .from("La suscripción mensual de Netflix subió a 12 euros."));
+        fewShotHistory.add(AiMessage.from(
+                "Producto: Suscripción Netflix, Precio: 12, Moneda: EUR, Sentimiento: NEGATIVO"));
+
+      
+        // Adding real user's message
+        UserMessage customerComplaint = UserMessage
+                .from("Ayer pagué 4500 guaranies por una hamburguesa doble con papas, me encantó");
+        fewShotHistory.add(customerComplaint);
+
+        System.out.println("[User]: " + customerComplaint.singleText());
+        System.out.print("[LLM]: ");
+
+        CompletableFuture<ChatResponse> futureChatResponse = new CompletableFuture<>();
+
+        chatService.getModel().chat(fewShotHistory, new StreamingChatResponseHandler() {
+
+            @Override
+            public void onPartialResponse(String partialResponse) {
+                System.out.print(partialResponse);
+            }
+
+            @Override
+            public void onCompleteResponse(ChatResponse completeResponse) {
+                futureChatResponse.complete(completeResponse);
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                futureChatResponse.completeExceptionally(error);
+            }
+        });
+
+        futureChatResponse.join();
+
+        // Extract reply and send to customer
+        // Perform necessary action in back-end
+    }
+}
---
+++ src/main/java/ollama/OllamaStreamingChatMemory.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ollama;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import java.util.concurrent.ExecutionException;
+import static dev.langchain4j.data.message.UserMessage.userMessage;
+import dev.langchain4j.model.chat.StreamingChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
+import java.util.concurrent.CompletableFuture;
+/**
+ *
+ * @author jmferreira
+ */
+public class OllamaStreamingChatMemory {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        OllamaStreamingChatService chatService
+                = new OllamaStreamingChatService(
+                        "http://10.254.3.242:11434",
+                        "gemma3:4b"
+                );
+
+        ChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .maxMessages(10) // últimos 10 mensajes
+                .build();
+
+        SystemMessage systemMessage = SystemMessage.from(
+                "You are a senior developer explaining to another senior developer, "
+                + "the project you are working on is an e-commerce platform with Java back-end, "
+                + "Oracle database, and Spring Data JPA");
+        chatMemory.add(systemMessage);
+
+        UserMessage userMessage1 = userMessage(
+                "How do I optimize database queries for a large-scale e-commerce platform? "
+                + "Answer short in two paragrahs maximum.");
+        chatMemory.add(userMessage1);
+
+        System.out.println("[User]: " + userMessage1.singleText());
+        System.out.print("[LLM]: ");
+
+        AiMessage aiMessage1 = streamChat(chatService.getModel(), chatMemory);
+        chatMemory.add(aiMessage1);
+
+        UserMessage userMessage2 = userMessage(
+                "Give a concrete example implementation of the first point? "
+                + "Be short, 10 lines of code maximum.");
+        chatMemory.add(userMessage2);
+
+        System.out.println("\n\n[User]: " + userMessage2.singleText());
+        System.out.print("[LLM]: ");
+
+        AiMessage aiMessage2 = streamChat(chatService.getModel(), chatMemory);
+        chatMemory.add(aiMessage2);
+    }
+
+    private static AiMessage streamChat(StreamingChatModel model, ChatMemory chatMemory)
+            throws ExecutionException, InterruptedException {
+
+        CompletableFuture<AiMessage> futureAiMessage = new CompletableFuture<>();
+
+        StreamingChatResponseHandler handler = new StreamingChatResponseHandler() {
+
+            @Override
+            public void onPartialResponse(String partialResponse) {
+                System.out.print(partialResponse);
+            }
+
+            @Override
+            public void onCompleteResponse(ChatResponse completeResponse) {
+                futureAiMessage.complete(completeResponse.aiMessage());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+            }
+        };
+
+        model.chat(chatMemory.messages(), handler);
+        return futureAiMessage.get();
+    }
+}
